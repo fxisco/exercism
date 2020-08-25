0b8e1da40918142d6f7acc4e781cacd27decbadb
Francisco Payés <payesfrancisco@gmail.com>
2020-08-25 17:59:04 +0200

    difference-of-squares: (improvement) avoid using math library for power of 2

--- go/difference-of-squares/difference_of_squares.go
+++ go/difference-of-squares/difference_of_squares.go
@@ -1,28 +1,20 @@
 package diffsquares
 
-import (
-	"math"
-)
-
-func SumOfSquares(n int) int {
-	var sumOfSq int
-
+func SumOfSquares(n int) (sumOfSq int) {
 	for i := 1; i <= n; i++ {
-		sumOfSq += int(math.Pow(float64(i), 2.0))
+		sumOfSq += (i * i)
 	}
 
-	return sumOfSq
+	return
 }
 
 func SquareOfSum(n int) int {
 	sum := float64((n * (n + 1)) / 2)
-	sqOfSum := int(math.Pow(sum, 2.0))
+	sqOfSum := int(sum * sum)
 
 	return sqOfSum
 }
 
 func Difference(n int) int {
-	difference := float64(SumOfSquares(n) - SquareOfSum(n))
-
-	return int(math.Abs(difference))
+	return SquareOfSum(n) - SumOfSquares(n)
 }
