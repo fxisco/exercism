441f0b51c9f8cdb1beaad58fdd2c73ce21ae3ad1
Francisco Payés <payesfrancisco@gmail.com>
2020-09-10 17:19:25 +0200

    tree-building: second solution, using maps to keep track of progress Missing: order tree children

--- go/tree-building/tree_building.go
+++ go/tree-building/tree_building.go
@@ -1,5 +1,7 @@
 package tree
 
+import "errors"
+
 type Record struct {
 	ID     int
 	Parent int
@@ -10,6 +12,54 @@ type Node struct {
 	Children []*Node
 }
 
-func Build(records []Record) (*Node, error) {
+func Build(records []Record) (root *Node, err error) {
+	if len(records) == 0 {
+		return nil, nil
+	}
 
+	r := make(map[int]Record)
+	m := make(map[int]*Node)
+
+	// Check for duplicates
+	// Check parent ID, shout be the same if node is 0 or
+	// should have a lower ID
+	for _, item := range records {
+		_, prs := m[item.ID]
+
+		if item.Parent >= item.ID && item.ID != 0 {
+			err = errors.New("Parent ID")
+			break
+		}
+
+		if !prs {
+			r[item.ID] = item
+			m[item.ID] = &Node{ID: item.ID}
+		} else {
+			err = errors.New("Repeated node")
+			break
+		}
+	}
+
+	if err != nil {
+		return
+	}
+	root, ok := m[0]
+
+	if len(m) == 1 && ok {
+		return root, nil
+	}
+
+	index := 0
+
+	for index < len(records) {
+		for _, v := range m {
+			if v.ID != 0 && r[v.ID].Parent == index {
+				m[index].Children = append(m[index].Children, v)
+			}
+		}
+
+		index++
+	}
+
+	return m[0], nil
 }
