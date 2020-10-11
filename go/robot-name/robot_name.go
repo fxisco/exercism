3bbe4f7035b6960edd0ea4d2b78202a2b59fa144
Francisco Payés <payesfrancisco@gmail.com>
2020-10-11 17:01:23 +0200

    robot-name: suggested solution

--- go/robot-name/robot_name.go
+++ go/robot-name/robot_name.go
@@ -1,57 +1,141 @@
+// Package robotname implements a type Robot with a unique Name field.
+
 package robotname
 
 import (
-	"fmt"
-	"math/rand"
+	"crypto/rand"
+	"errors"
+	"math/big"
 )
 
 type Robot struct {
-	RobotName string
+	name string
 }
 
 const (
-	MIN = 65
-	MAX = 90
+	upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	digit     = "0123456789"
 )
 
-var assigned = map[string]int{}
-
-func (r *Robot) Name() (string, error) {
-	if len(r.RobotName) > 0 {
-		return r.RobotName, nil
-	}
-
-	var result string
-	finish := false
-
-	for !finish {
-		result = ""
-
-		for i := 0; i < 2; i++ {
-			first := rand.Intn(MAX-MIN) + MIN
-			result += string(rune(first))
-		}
-
-		item, _ := assigned[result]
-		item++
-
-		if item < 1000 {
-			result = fmt.Sprintf("%s%03d", result, item)
-			finish = true
-		}
-	}
-
-	r.RobotName = result
-
-	return result, nil
+// digitSpec specifies valid digit values for a multi-base string from most
+// significant digit to least significant digit.
+var digitSpec = []string{
+	upperChar,
+	upperChar,
+	digit,
+	digit,
+	digit,
 }
 
-func (r *Robot) Reset() (string, error) {
-	r.RobotName = ""
+// digitIncrements stores the increment value of each digit position for later
+// use during the encoding step.
+var digitIncrements = make([]int, len(digitSpec))
 
-	newName, err := r.Name()
+// MaxNames is the maximum number of names that can be generated based on the
+// defined digitSpec.
+var MaxNames int
 
-	r.RobotName = newName
+// Keep track of unused integer IDs. These are intended to be used to
+// deterministically generate new robot names, and then be removed from this
+// slice.
+var availableIDs []int
 
-	return r.RobotName, err
+// init initializes values for various global variables.
+func init() {
+	var m int
+
+	m = 1
+	numDigits := len(digitSpec)
+	for i := numDigits - 1; i >= 0; i-- {
+		digitIncrements[i] = m
+		m *= len(digitSpec[i])
+	}
+	MaxNames = m
+
+	availableIDs = make([]int, MaxNames)
+	for i := 0; i < MaxNames; i++ {
+		availableIDs[i] = i
+	}
+}
+
+// randIntn wraps the crypto/rand integer generator to return a random
+// int.
+func randIntn(i int) int {
+	bigi := big.NewInt(int64(i))
+	rndm, _ := rand.Int(rand.Reader, bigi)
+	return int(rndm.Int64())
+}
+
+// Given an integer "ID", encode it into a multi-base string according to the
+// given list of strings, digitSpec, where each string represents valid symbols
+// for its digit position in order of increasing value. The digit spec
+// describes digits in this way in order of most significant to least
+// significant, ie index 0 is the most significant digit, index 1 is the next
+// most significant, etc
+func intToMultiBaseString(id int, digitSpec []string) string {
+	var pos, rmndr, minVal int
+	var result string
+
+	rmndr = id
+	for i, digitChars := range digitSpec {
+		minVal = digitIncrements[i]
+
+		// The character for this digit position is determined by the number of
+		// times that the minimum/increment value of that digit can evenly
+		// divide into the remainder from the last digit's calculations (or the
+		// original ID in the case of the most significant digit).
+		pos = rmndr / minVal
+		result += string(digitChars[pos])
+
+		// Calculate the remainder to be used for the next digit.
+		rmndr %= minVal
+	}
+
+	return result
+}
+
+// getUniqueID returns a random unused ID and panics if none are left.
+func getUniqueID() (int, error) {
+	numAvailable := len(availableIDs)
+	if numAvailable == 0 {
+		return 0, errors.New("all possible names have been taken")
+	}
+
+	i := randIntn(numAvailable)
+	uniqueID := availableIDs[i]
+
+	// Delete the ith element by replacing it with the last element in the
+	// array and cutting off the original last element.
+	availableIDs[i] = availableIDs[numAvailable-1]
+	availableIDs = availableIDs[:numAvailable-1]
+
+	return uniqueID, nil
+}
+
+// generateUniqueName panics if there are no valid unique names left, returns a
+// valid unique name otherwise.
+func generateUniqueName() (string, error) {
+	id, err := getUniqueID()
+	if err != nil {
+		return "", err
+	}
+
+	return intToMultiBaseString(id, digitSpec), nil
+}
+
+// Name returns the name of the robot.
+func (r *Robot) Name() (string, error) {
+	var err error
+	if r.name == "" {
+		r.name, err = generateUniqueName()
+		if err != nil {
+			return "", err
+		}
+	}
+	return r.name, nil
+}
+
+// Reset resets the robot's name to empty string.
+func (r *Robot) Reset() {
+	*r = Robot{}
 }
