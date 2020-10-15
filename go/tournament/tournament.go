36195efac4ff06cddc19e03535777532143d3757
Francisco Payés <payesfrancisco@gmail.com>
2020-10-15 23:07:37 +0200

    tournament: sort results

--- go/tournament/tournament.go
+++ go/tournament/tournament.go
@@ -5,10 +5,12 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"sort"
 	"strings"
 )
 
 type register struct {
+	name string
 	win  int
 	draw int
 	loss int
@@ -20,6 +22,21 @@ func (reg *register) getMatchPlayed() int {
 
 func (reg *register) getPoints() int {
 	return 3*reg.win + reg.draw
+}
+
+type ByPoints []*register
+
+func (a ByPoints) Len() int      { return len(a) }
+func (a ByPoints) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
+func (a ByPoints) Less(i, j int) bool {
+	switch {
+	case a[i].getPoints() != a[j].getPoints():
+		return a[i].getPoints() > a[j].getPoints()
+	case a[i].win != a[j].win:
+		return a[i].win > a[j].win
+	default:
+		return a[i].name < a[j].name
+	}
 }
 
 func getPunctuationsTable(reader io.Reader) (error, map[string]*register) {
@@ -41,17 +58,21 @@ func getPunctuationsTable(reader io.Reader) (error, map[string]*register) {
 			return err, nil
 		}
 
+		if len(record) != 3 {
+			return errors.New("INPUT: Bad format"), nil
+		}
+
 		teamA, prsA := m[record[0]]
 		teamB, prsB := m[record[1]]
 
 		action := strings.TrimSuffix(record[2], "\n")
 
 		if !prsA {
-			teamA = &register{}
+			teamA = &register{name: record[0]}
 		}
 
 		if !prsB {
-			teamB = &register{}
+			teamB = &register{name: record[1]}
 		}
 
 		if action == "win" {
@@ -60,9 +81,11 @@ func getPunctuationsTable(reader io.Reader) (error, map[string]*register) {
 		} else if action == "loss" {
 			teamA.loss++
 			teamB.win++
-		} else {
+		} else if action == "draw" {
 			teamA.draw++
 			teamB.draw++
+		} else {
+			return errors.New("GAME RESULT: Bad format"), nil
 		}
 
 		m[record[0]] = teamA
@@ -81,8 +104,17 @@ func Tally(reader io.Reader, buffer io.Writer) error {
 
 	buffer.Write([]byte("Team                           | MP |  W |  D |  L |  P\n"))
 
-	for k, v := range table {
-		s := fmt.Sprintf("%-31s| %2d | %2d | %2d | %2d | %2d\n", k, v.getMatchPlayed(), v.win, v.draw, v.loss, v.getPoints())
+	// convert map to array
+	values := []*register{}
+
+	for _, value := range table {
+		values = append(values, value)
+	}
+
+	sort.Sort(ByPoints(values))
+
+	for _, v := range values {
+		s := fmt.Sprintf("%-31s| %2d | %2d | %2d | %2d | %2d\n", v.name, v.getMatchPlayed(), v.win, v.draw, v.loss, v.getPoints())
 		buffer.Write([]byte(s))
 	}
 
